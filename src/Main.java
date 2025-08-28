2e44ff5cdc75e5a0d291c33b0219d5841f978c73
jfiquitiva12 <johanfiquitiva5@gmail.com>
2025-08-28 12:30:35 -0500

    actualizacion 1.3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola clase");
+        System.out.printf("Aprendiendo Git");
+        System.out.println("1. Git status (Para ver el status de git)");
+        System.out.println("2. Git add (Agregar archivos).");
+        System.out.printf("3. Git push origin () ");
+        System.out.printf("4. Git clone (Clonar proyectos)");
+        System.out.printf("5. Git switch (crear y posicionarnos sobre la rama)");
+        System.out.printf("6. Git fetch");
 
     }
 }
